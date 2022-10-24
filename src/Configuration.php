a5c8bd861092418c1581273776be2aa270ca0501
Jan Barášek <janbarasek@gmail.com>
2022-10-24 10:27:52 +0200

    Fix coding standard.

--- src/Configuration.php
+++ src/Configuration.php
@@ -22,17 +22,15 @@ final class Configuration
 		public bool $trim = false,
 		public ?int $cols = null,
 		public ?int $rows = null,
-		public bool $bestfit = false
+		public bool $bestfit = false,
 	) {
 		$this->format = strtolower($format);
 		if (in_array($this->format, self::SupportedFormats, true) === false) {
-			throw new \InvalidArgumentException(
-				sprintf(
-					'Format "%s" is not supported. Did you mean "%s"?',
-					$this->format,
-					implode('", "', self::SupportedFormats),
-				)
-			);
+			throw new \InvalidArgumentException(sprintf(
+				'Format "%s" is not supported. Did you mean "%s"?',
+				$this->format,
+				implode('", "', self::SupportedFormats),
+			));
 		}
 		if (is_file($pdfPath) === false) {
 			throw new ConvertorException(sprintf('File "%s" does not exist.', $pdfPath));
